9c6e5e603967080b19c5561e1277151e4ce65c1e
zpalms91 <zoepalmer1991@hotmail.com>
2020-06-30 22:35:24 -0500

    Added identity

--- BelleEssense.UI/Models/AccountViewModels.cs
+++ BelleEssense.UI/Models/AccountViewModels.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace BelleEssense.UI.Models
+namespace IdentitySample.Models
 {
     public class ExternalLoginConfirmationViewModel
     {
--- BelleEssense.UI/Startup.cs
+++ BelleEssense.UI/Startup.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(BelleEssense.UI.Startup))]
-namespace BelleEssense.UI
+namespace IdentitySample
 {
     public partial class Startup
     {
